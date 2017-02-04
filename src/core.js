7e2ded8e58aa38720c3e715ea31c442bb955132f
Daniella Baxter <dbaxter@nyc-047571-mbp.local>
2017-02-03 20:13:31 -0500

    add reducers

--- src/core.js
+++ src/core.js
@@ -1,20 +1,14 @@
 import {List, Map} from 'immutable';
 
+export const INITIAL_STATE = Map();
+
 export function setEntries(state, entries) {
   return state.set('entries', List(entries));
 }
 
-// export function next(state) {
-//   const entries = state.get('entries');
-//   return state.merge({
-//     vote: Map({pair: entries.take(2)}),
-//     entries: entries.skip(2)
-//   });
-// }
-
-export function vote(state, entry) {
-  return state.updateIn(
-    ['vote', 'tally', entry],
+export function vote(voteState, entry) {
+  return voteState.updateIn(
+    ['tally', entry],
     0,
     tally => tally + 1
   );
